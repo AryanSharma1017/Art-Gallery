5505015d77ddd8c0765528b0fa03e28412d5e662
Aryan Sharma <aryansharma772004@gmail.com>
2024-04-24 09:31:00 +0530

    Updation

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,8 +2,27 @@ using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 using art_gallery.Models;
 using art_gallery.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+});
 
 builder.Services.AddCors(options =>
 {
